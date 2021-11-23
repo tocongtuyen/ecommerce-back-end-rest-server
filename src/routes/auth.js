94525013063dfd49822493869a1578f8f5255f17
TO CONG TUYEN <tocongtuyen@gmail.com>
2021-11-23 08:31:11 +0700

    Api request validation - 5

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { signup, signin } = require('../controllers/auth');
+const { validateSignupRequest, isRequestValidated, validateSigninRequest } = require('../validators/auth');
 const router = express.Router();
 
 
-router.post('/signup', signup);
-router.post('/signin', signin);
+router.post('/signup', validateSignupRequest, isRequestValidated, signup);
+router.post('/signin', validateSigninRequest, isRequestValidated, signin);
 
 // router.post('/profile', (req, res) => {
 //     res.status(200).json({ user: 'profile' })
