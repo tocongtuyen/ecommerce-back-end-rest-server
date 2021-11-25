8bb32f1a9c8b42349601e3f16d47413955056085
TO CONG TUYEN <tocongtuyen@gmail.com>
2021-11-25 14:23:10 +0700

    Product creation api - 7

---
+++ src/routes/product.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const multer = require("multer");
+const router = express.Router();
+const shortid = require("shortid");
+const path = require("path");
+const { requireSignin, adminMiddleware } = require("../common-middleware");
+const { createProduct } = require("../controllers/product");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(path.dirname(__dirname), "uploads"));
+    },
+    filename: function (req, file, cb) {
+        cb(null, shortid.generate() + "-" + file.originalname);
+    },
+});
+
+const upload = multer({ storage });
+
+router.post("/product/create", requireSignin, adminMiddleware, upload.array('productPicture'), createProduct);
+
+module.exports = router;
